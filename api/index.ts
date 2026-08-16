ad2c7f2e1433571e2caad74f0ac1af2b18b9030c
Myeongjun <bluejun0901@gmail.com>
2026-08-17 00:50:54 +0900

    Update import statement in index.ts to use the correct runtime extension for Node ESM

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,5 @@
-import app from '../server';
+// Use the runtime extension for Node ESM. TypeScript resolves this to
+// server.ts during compilation, while Vercel emits and loads server.js.
+import app from '../server.js';
 
 export default app;
